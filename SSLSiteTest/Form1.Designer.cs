a8d92f68a6eda371c5f1f6b97b97f407a1c14781
astrickland15 <58783938+astrickland15@users.noreply.github.com>
2021-04-15 18:10:27 -0400

    Add files via upload

---
+++ SSLSiteTest/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace SSLSiteTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Sites = new System.Windows.Forms.TextBox();
+            this.check = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // Sites
+            // 
+            this.Sites.Location = new System.Drawing.Point(56, 29);
+            this.Sites.Multiline = true;
+            this.Sites.Name = "Sites";
+            this.Sites.Size = new System.Drawing.Size(466, 354);
+            this.Sites.TabIndex = 0;
+            // 
+            // check
+            // 
+            this.check.Location = new System.Drawing.Point(588, 109);
+            this.check.Name = "check";
+            this.check.Size = new System.Drawing.Size(75, 23);
+            this.check.TabIndex = 1;
+            this.check.Text = "Check Sites";
+            this.check.UseVisualStyleBackColor = true;
+            this.check.Click += new System.EventHandler(this.check_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(56, 411);
+            this.progressBar.Minimum = 1;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(466, 23);
+            this.progressBar.TabIndex = 2;
+            this.progressBar.Value = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.check);
+            this.Controls.Add(this.Sites);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Sites;
+        private System.Windows.Forms.Button check;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ SSLSiteTest/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSLSiteTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void check_Click(object sender, EventArgs e)
+        {
+            List<string> unResolvedSites = new List<string>();
+            progressBar.Maximum = Sites.Lines.Length;
+
+            if (string.IsNullOrEmpty(Sites.Text))
+            {
+                MessageBox.Show(string.Format("Please Enter Some Domains."), "Missing Domains");
+                    return;
+            }
+
+            foreach (var site in Sites.Lines)
+            {
+                try
+                {
+                    if (string.IsNullOrEmpty(site))
+                    {
+                        continue;
+                    }
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(SecureLink(site));
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    progressBar.Increment(1);
+                }
+                catch
+                {
+                    unResolvedSites.Add(site);
+                }               
+            }
+            
+            if (unResolvedSites.Count == 0)
+            {
+                MessageBox.Show(string.Format("Congratulations!  All Sites Resolve."), "Break Out The Champagne");
+            }
+            var siteList = string.Join(Environment.NewLine, unResolvedSites);
+
+            if (unResolvedSites.Count > 0)
+            {
+                MessageBox.Show(string.Format("The Following {0} Sites Did Not Resolve: \n\n " + siteList, unResolvedSites.Count), "Summary");
+                progressBar.Increment(unResolvedSites.Count);
+            }
+            AddToTextFile(unResolvedSites);
+        }
+
+        public static string SecureLink(string link)
+        {
+            return "https://www." + link;
+        }
+
+        public static void AddToTextFile(List<string> domains)
+        {
+            using (StreamWriter sw = new StreamWriter(@"c:\testing\UnresolvedSSLDomains.txt"))
+            {
+                foreach (string domain in domains)
+                {
+                    sw.WriteLine(domain);
+                }
+            }
+        }
+    }
+}
